009672cd336e4e14c8276fbe5ee1d7c172571ca7
Jeremy Schmidt <8888694+nocturnalBadger@users.noreply.github.com>
2021-05-03 18:44:37 -0400

    More rusty code for finding tty port, update claw limits, more things I don't care to type

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,15 @@ extern crate sdl2;
 use std::time::{Duration, Instant};
 use std::str;
 
-const LIMITS: [(f32, f32); 3] = [
+const LIMITS: [(f32, f32); 4] = [
     (0.0, 180.0),  // Base rotation
     (60.0,150.0),  // Lower joint
     (40.0, 120.0), // Upper joint
+    (72.0, 180.0), // Claw
 ];
+
+const ANALOG_DEADSPOT: i16 = 1000;
+const SERIAL_BAUDRATE: u32 = 57600;
 
 fn main() -> Result<(), String> {
     ctrlc::set_handler( move || {std::process::exit(0)} ).expect("Error setting handler");
@@ -19,14 +23,21 @@ fn main() -> Result<(), String> {
 
     println!("{} joysticks available", available);
 
+    let mut port = {
+        let available_ports = serialport::available_ports().expect("No ports found!");
 
-    let mut port = serialport::new("/dev/ttyUSB1", 57600)
-        .timeout(Duration::from_millis(10))
-        .open().expect("Failed to open port");
+        match available_ports.iter().find(|x| x.port_name.contains("ttyUSB")) {
+            Some(port_info) => Ok(serialport::new(port_info.port_name.clone(), SERIAL_BAUDRATE).open().expect("Failed to open port")),
+            None => Err("No serial port found"),
+        }
+    }.expect("Error getting serial port");
 
-    let update_millis = 50;
+
+
+
+    let update_millis = 25;
     let update_time = Duration::from_millis(update_millis);
-    let max_speed: f32 = 2.0 / (update_millis as f32 / 1000.0);
+    let max_speed: f32 = 1.0 / (update_millis as f32 / 1000.0);
     println!("{}", max_speed);
     let mut last_write_time = Instant::now();
 
@@ -55,8 +66,9 @@ fn main() -> Result<(), String> {
         })
         .expect("Couldn't open any controller");
 
-    let mut pos: [f32; 3] = [90.0, 90.0, 90.0];
-    let mut speed: [f32; 3] = [0.0, 0.0, 0.0];
+    let mut pos: [f32; 4] = [90.0, 90.0, 90.0, 90.0];
+    let mut last_pos = pos;
+    let mut speed: [f32; 4] = [0.0, 0.0, 0.0, 0.0];
 
     println!("Controller mapping: {}", controller.mapping());
 
@@ -76,7 +88,7 @@ fn main() -> Result<(), String> {
                     //
 
                     let stick_speed = {
-                        if val > 100 || val < 100 {
+                        if val > ANALOG_DEADSPOT || val < -ANALOG_DEADSPOT {
                             (val as f32 / 32767.0) * max_speed
                         } else { 0.0 }
                     };
@@ -86,6 +98,7 @@ fn main() -> Result<(), String> {
                         Axis::LeftX => speed[0] = stick_speed,
                         Axis::LeftY => speed[2] = stick_speed,
                         Axis::RightY => speed[1] = stick_speed,
+                        Axis::RightX => speed[3] = stick_speed,
                         _ => (),
                     }
                 }
@@ -93,11 +106,16 @@ fn main() -> Result<(), String> {
                     match button {
                         Button::A => {
                             println!("It's the A button!");
-                            pos = [130.0, 130.0, 130.0]
-
+                            if pos[3] != LIMITS[3].1 {
+                                pos[3] = LIMITS[3].1;
+                            } else {
+                                pos[3] = LIMITS[3].0;
+                            }
                         }
-                        Button::B => {
-                            pos = [90.0, 90.0, 90.0];
+                        Button::Guide => {
+                            println!("Setting position to home");
+                            pos = [90.0, 90.0, 90.0, 90.0];
+                            speed = [0.0, 0.0, 0.0, 0.0];
                         }
                         _ => (),
                     }
@@ -109,7 +127,7 @@ fn main() -> Result<(), String> {
                 _ => (),
             }
         }
-        //println!("End position: {:?}", pos);
+        // println!("End position: {:?}", pos);
         // println!("Speed: {:?}", speed);
         if last_write_time.elapsed() > update_time {
             for i in 0..3 {
@@ -121,15 +139,25 @@ fn main() -> Result<(), String> {
                 pos[i] = new_pos;
             }
 
-            println!("New position is {} {} {}", pos[0], pos[1], pos[2]);
-            let output_bytes = [pos[0] as u8, pos[1] as u8, pos[2] as u8];
-            port.write(&output_bytes).expect("write failed");
-            last_write_time = Instant::now();
+            if pos != last_pos {
+
+                println!("New position is {} {} {} {}", pos[0], pos[1], pos[2], pos[3]);
+                let output_bytes = [pos[0] as u8, pos[1] as u8, pos[2] as u8, pos[3] as u8];
+
+                port.write(&output_bytes).expect("write failed");
+                last_write_time = Instant::now();
+                last_pos = pos;
+            }
         }
 
         let mut serial_buf: Vec<u8> = vec![0; 32];
         match port.read(serial_buf.as_mut_slice()) {
-            Ok (_) => print!("{}", str::from_utf8(&serial_buf).expect("string conversion failed")),
+            Ok (_) => {
+                match str::from_utf8(&serial_buf) {
+                    Ok(message) => print!("{}", message),
+                    Err(_) => println!("Error parsing serial input"),
+                }
+            },
             Err (_) => (),
         };
 
